eff651521727ebc7bc4c49d79f44b268bdee6c82
Basharat Hussain <basharat58@hotmail.com>
2020-05-06 16:43:33 +0100

    Add the Xamarin and test projects.

---
+++ src/FlightBooking/FlightBooking/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FlightBooking.Models;
+
+namespace FlightBooking.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
